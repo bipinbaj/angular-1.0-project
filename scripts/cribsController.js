042792a6951edfa8907f9acee00563370320e51b
Bipin Bajracharya <bazrac40@gmail.com>
2016-12-03 18:26:49 +0545

    first push

---
+++ scripts/cribsController.js
@@ -0,0 +1,27 @@
+angular
+    .module('ngCribs')
+    .controller('cribsController', function($scope, cribsFactory) {
+
+    	$scope.cribs;
+
+    	$scope.priceInfo = {
+
+    		min: 0,
+    		max: 1000000
+    	}
+
+        $scope.newListing = {};
+
+        $scope.addCrib = function(newListing) {
+            $scope.cribs.push(newListing);
+        }
+
+
+    	cribsFactory.getCribs().success(function(data){
+    		$scope.cribs = data;
+    	}).error(function(error) {
+    		console.log(error);
+
+    	});
+
+    });
